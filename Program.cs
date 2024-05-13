e709ea2c8a3a781ed41ee3eca6407fda77ea266d
Sathyajith Bhat <sathya@sathyasays.com>
2024-05-13 19:36:03 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+class SpotifySync
+{
+    static async Task Main()
+    {
+        Console.WriteLine("Hello, world.");
+    }
+}
